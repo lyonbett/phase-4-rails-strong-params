f697b7a6b9b93adf4c752f4ecf79c89f62c6490c
lyonbett <lyon.bett@student.moringaschool.com>
2023-07-01 13:34:44 +0300

    completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,25 +1,22 @@
 class BirdsController < ApplicationController
 
+  wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
-    render json: birds
+@@ -22,4 +23,15 @@ def show
+    end
   end
 
-  # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
-  # GET /birds/:id
-  def show
-    bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
